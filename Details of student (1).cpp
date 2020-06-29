8694ba0ea58f617899fbbfb191617b265ba94887
shafreenasharief <53473425+shafreenasharief@users.noreply.github.com>
2020-06-29 21:41:06 +0530

    29 JUNE 2020

---
+++ Details of student (1).cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	private:
+		
+		char name[20],regd[10],branch[10];
+		int sem;
+	public:
+		void input()
+		{
+	        cout<<"Enter Name:";
+	        cin>>name;
+	        cout<<"Enter Regdno.:";
+	        cin>>regd;
+	        cout<<"Enter Branch:";
+	        cin>>branch;
+	        cout<<"Enter Sem:";
+	        cin>>sem;
+        }
+		void display()
+		{
+	        cout<<"\nName:"<<name;
+	        cout<<"\nRegdno.:"<<regd;
+	        cout<<"\nBranch:"<<branch;
+        	cout<<"\nSem:"<<sem;
+        }
+};
+int main()
+{
+	student s;
+	s.input();
+	s.display();
+}
